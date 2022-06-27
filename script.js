80b11f6bf726d0884b5fe29ec7bb643a652eb486
omaranBazna <omran4d@gmail.com>
2022-06-26 21:14:48 -0400

    add correct some code mistakes

--- script.js
+++ script.js
@@ -6,21 +6,21 @@ class jQuery{
   
    setContent(str){
     document.getElementById(this.id).innerHTML = str;
-    return document.getElementById(this.id);
+    return document.getElementById(this);
    }
    
    append(str){
     document.getElementById(this.id).innerHTML +=str;
       
-    return document.getElementById(this.id);
+    return document.getElementById(this);
    }
    setColor(color){
     document.getElementById(this.id).style.color = color;
-    return document.getElementById(this.id);
+    return document.getElementById(this);
    }
    setBackgroundColor(color){
-    document.getElementById(this.id).style.backgroundColor = color;
-    return document.getElementById(this.id);
+    document.getElementById(this.id).style.backgroundColor =color
+    return document.getElementById(this);
    }
 }
 
@@ -28,4 +28,5 @@ function $(str){
     const jQ=new jQuery(str);
     return jQ;
 }
-$("test").setContent("hello new jQuery").append(" : I append this text").setBackgroundColor("green")
+$("test").setContent("hello new jQuery").append(" : I append this text")
+$("test").setBackgroundColor("green")
