1f672a5a08325178400978678c7c515cbedf76dc
omaranBazna <omran4d@gmail.com>
2022-06-26 20:44:19 -0400

    implement the set content funtion to change the content of the element

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 class jQuery{
    constructor(id){
     this.element=document.getElementById(id)
-    return this.element;
+    
    }
-  /*
+  
    setContent(str){
     this.element.innerHTML = str;
     return this.element;
    }
+   /*
    append(str){
     this.element.innerHTML +=str;
     return this.element;
    }
    */
 }
+
 function $(str){
     const jQ=new jQuery(str);
     return jQ;
 }
-$("test").innerHTML= "hello jQuery world";
+$("test").setContent("hello new jQuery")
