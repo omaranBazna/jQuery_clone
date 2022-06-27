525fe1a593c9105994c2b862737cc1fd7b94a131
omaranBazna <omran4d@gmail.com>
2022-06-26 20:45:31 -0400

    implmen the append function

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ class jQuery{
     this.element.innerHTML = str;
     return this.element;
    }
-   /*
+   
    append(str){
     this.element.innerHTML +=str;
     return this.element;
    }
-   */
+   
 }
 
 function $(str){
     const jQ=new jQuery(str);
     return jQ;
 }
-$("test").setContent("hello new jQuery")
+$("test").setContent("hello new jQuery").append(" : I append this text")
