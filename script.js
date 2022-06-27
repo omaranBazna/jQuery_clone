c21340cd36930cf98195a52324dbfd2f2bcc9a71
omaranBazna <omran4d@gmail.com>
2022-06-26 21:12:21 -0400

    add setBackgroundColor method

--- script.js
+++ script.js
@@ -18,11 +18,14 @@ class jQuery{
     document.getElementById(this.id).style.color = color;
     return document.getElementById(this.id);
    }
+   setBackgroundColor(color){
+    document.getElementById(this.id).style.backgroundColor = color;
+    return document.getElementById(this.id);
+   }
 }
 
 function $(str){
     const jQ=new jQuery(str);
     return jQ;
 }
-$("test").setContent("hello new jQuery").append(" : I append this text")
-$("test").setColor()
+$("test").setContent("hello new jQuery").append(" : I append this text").setBackgroundColor("green")
